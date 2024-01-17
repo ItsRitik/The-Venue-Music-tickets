c6bab3231d9d7adb4e35d95df3eef80570b93bc4
ItsRitik <65969779+ItsRitik@users.noreply.github.com>
2024-01-17 03:25:03 -0500

    Implemented Carousel and time interval

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "../src/resources/styles.css"
 import Header from "../src/components/header_footer/Header"
 import Footer from "./components/header_footer/Footer"
+import Featured from "./components/featured/index"
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Featured />
       <Footer />
     </div>
   );
